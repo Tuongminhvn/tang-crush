7a10d292603ac4dec6d6495c3b418de7bf9d945a
Tuongminhvn <86848370+Tuongminhvn@users.noreply.github.com>
2021-07-03 10:39:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
     messDesc: 'Yêuuuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/100012726643285' //link mess của các bạn. VD: https://m.me/nam.nodemy
